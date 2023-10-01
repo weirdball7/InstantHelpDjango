150a4c4a81a5b79528ae5372983a6168800c8e52
WeirdBall77 <120051405+WeirdBall77@users.noreply.github.com>
2023-10-01 14:38:18 +0300

    added second page-Not sure if working

--- InstantHelp/urls.py
+++ InstantHelp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.home, name="home")
+    path("home/", views.home, name="home"),
+    path("contact/", views.contact, name="contact"),
 ]
 
--- InstantHelp/views.py
+++ InstantHelp/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, "html/homepage.html")
+
+def connect(request):
+    return render(request, "html/therapiest-contact.html")
     
